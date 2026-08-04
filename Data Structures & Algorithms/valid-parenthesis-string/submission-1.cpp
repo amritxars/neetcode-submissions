e62697904443915d66202636056ff954d90cf8ea
Amrit <157050961+amritxars@users.noreply.github.com>
2026-08-04 18:06:24 +0530

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int left = 0;
+        int right = 0;
+
+        for(char ch : s){
+            
+            if(ch == '('){
+                left++;
+                right++;
+            }
+
+            if(ch == ')'){
+                left--;
+                right--;
+            }
+
+            if(ch == '*'){
+                left--;
+                right++;
+            }
+
+            if(right<0) return false;
+
+            if(left<0) left = 0;
+        }
+
+        return left == 0
+        ;
+    }
+};
