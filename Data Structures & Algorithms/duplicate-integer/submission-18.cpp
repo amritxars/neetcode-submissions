f3220ec49984fbfee9752664b53d24116f174d94
Amrit <157050961+amritxars@users.noreply.github.com>
2026-08-26 00:02:09 +0530

    Add: duplicate-integer - submission-18

---
+++ Data Structures & Algorithms/duplicate-integer/submission-18.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int,int> mp;
+
+        for(int n : nums){
+            mp[n]++;
+        }
+
+        for(auto it : mp){
+            if(it.second > 1) return true;
+        }
+
+        return false;
+    }
+};
